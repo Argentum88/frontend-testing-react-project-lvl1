91801cda57de7a455a31b8be030018aac054f681
Argentum88 <argentimgipan@gmail.com>
2021-05-02 16:45:52 +0500

    init

---
+++ __tests__/sum.test.js
@@ -0,0 +1,5 @@
+import sum from '../src/sum.js';
+
+test('sum', () => {
+  expect(sum(3, 2)).toEqual(5);
+});
---
+++ bin/page-loader.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import sum from '../src/sum.js';
+
+console.log(sum(2, 3));
