24e145aac76b2c03b508dce9bf9aba7b36706ae5
Argentum88 <argentimgipan@gmail.com>
2021-05-12 00:12:41 +0500

    step5

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,13 +4,20 @@ import { cwd } from 'process';
 import { program } from 'commander';
 import load from '../src/loader.js';
 
+const main = async (url, options) => {
+  try {
+    const { output } = options;
+    console.log(await load(url, output));
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1);
+  }
+};
+
 program
   .description('Site loader')
   .arguments('<url>')
   .option('--output <v>', 'output path', cwd())
-  .action(async (url, options) => {
-    const { output } = options;
-    console.log(await load(url, output));
-  });
+  .action(main);
 
 (async () => program.parseAsync(process.argv))();
